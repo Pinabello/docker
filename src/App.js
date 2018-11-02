270c0805d138f176fd3ada89d2112f13955ad4b0
Paolo Luciani <pinabello@gmail.com>
2018-11-02 19:55:17 +0100

    Travis config AWS expose

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Ti ho fregato!
+            Ti ho fregato e cambiato!
           </p>
           <a
             className="App-link"
